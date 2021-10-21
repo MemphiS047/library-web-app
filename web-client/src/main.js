fb3bfc355df1f0be5b4aca41dc1b241396c10dc8
ygt <hakverdiyigit@gmail.com>
2021-10-21 12:58:22 +0300

    Added bootstrap to Vue-2 (Replaced it with Vue-3 since bootstrap-vue does not support version 3)

--- web-client/src/main.js
+++ web-client/src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Bootstrap imports
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
