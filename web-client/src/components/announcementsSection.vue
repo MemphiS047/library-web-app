8f4da9750dc4f3234a1e1b57426fc43afca160f1
ygt <hakverdiyigit@gmail.com>
2021-11-08 20:36:34 +0300

    Announcement div and announcement rows templates changed added flex prop.

---
+++ web-client/src/components/announcementsSection.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="announcemenetsDiv">
+    <span class="announcementTitle">A Guide on How to Use Library Books</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "announcementsSection",
+};
+</script>
+
+<style>
+.announcemenetsDiv {
+  /* Frame 24 */
+
+  width: 100%;
+  height: 209px;
+  padding: 30px;
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  border-radius: 10px;
+}
+.announcementTitle {
+  /* A Guide on How to Use Library Books */
+
+  width: 419px;
+  height: 27px;
+  left: 27px;
+  top: 39px;
+
+  font-family: Manjari;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 25px;
+  line-height: 27px;
+
+  color: #000000;
+}
+</style>
--- web-client/src/components/mainpageAnnouncements.vue
+++ web-client/src/components/mainpageAnnouncements.vue
@@ -1,34 +1,42 @@
 <template>
-  <div class="announcementsDiv">
-    <span>Announcements & News</span>
+  <div>
+    <div class="announcementsTitleDiv">
+      <span>Announcements & News</span>
+    </div>
+    <div id="announcementsID" class="container">
+      <div class="row announcementsRow">
+        <announcementsSection />
+      </div>
+      <div class="row announcementsRow">
+        <announcementsSection />
+      </div>
+      <div class="row announcementsRow">
+        <announcementsSection />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import announcementsSection from "./announcementsSection.vue";
+
 export default {
   name: "mainpageAnnouncements",
+  components: {
+    announcementsSection,
+  },
 };
 </script>
 
 <style>
-.announcementsDiv {
-  margin-top: 20px;
-  height: 1330px;
-
-  position: relative;
-  width: 1337px;
-  height: 1127px;
-
-  border: 1px solid #000000;
-  box-sizing: border-box;
-  border-radius: 10px;
+.announcementsTitleDiv {
+  margin-top: 50px;
+  margin-left: 10px;
 }
-
 span {
   /* Anouncements & News */
 
-  position: absolute;
-  width: 500px;
+  width: 400px;
   height: 33px;
   left: 31px;
   top: 33px;
@@ -42,5 +50,20 @@ span {
   /* identical to box height */
 
   color: #000000;
+}
+.announcementsRow {
+  padding: 15px;
+}
+#announcementsID {
+  /* Frame 20 */
+  margin-top: 50px;
+  margin-bottom: 50px;
+  position: relative;
+  width: 1337px;
+
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  border-radius: 10px;
+  padding-bottom: 35px;
 }
 </style>
