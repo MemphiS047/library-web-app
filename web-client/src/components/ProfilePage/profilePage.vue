7c49078bf5d9195324dac3c1a99fd83543709a19
yigit <hakverdiyigit@gmail.com>
2021-12-07 20:06:35 +0300

    Profile page banner section done

--- web-client/src/components/ProfilePage/profilePage.vue
+++ web-client/src/components/ProfilePage/profilePage.vue
@@ -1,20 +1,77 @@
-<template> 
-<div>
+<template>
+  <div>
     <navbar />
     <secondarynavbar />
-</div>
+    <div class="container">
+      <div class="row">
+        <div class="col-3"></div>
+        <div class="col-10">
+          <profilePageBanner />
+        </div>
+        <div class="col-3"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import navbar from "./../navbar.vue";
 import secondarynavbar from "./../secondarynavbar.vue";
+import profilePageBanner from "./profilepageBanner.vue";
 export default {
   name: "profilePage",
   components: {
     navbar,
-    secondarynavbar
+    secondarynavbar,
+    profilePageBanner
   },
 };
 </script>
 
-<style></style>
+<style>
+.list-group {
+  margin-top: 120px;
+}
+
+.list-group-item {
+  background-color: transparent !important;
+  font-family: "Manjari";
+  padding: 10px 10px 10px 55px !important;
+}
+.profilePageSection {
+  position: relative;
+  width: 1317px;
+  height: 600px;
+  background: rgba(13, 30, 99, 0.15);
+  border: 3px solid #0d1e63;
+  box-sizing: border-box;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 5px;
+}
+
+.profilePageSectionTitle {
+  position: absolute;
+  width: 653px;
+  height: 33px;
+  left: 56px;
+  top: 33px;
+  padding-top: 15px;
+  font-family: Manjari;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 45px;
+  line-height: 33px;
+  color: #000000;
+}
+.profileBox {
+  position: relative;
+  width: 1200px;
+  height: 400px;
+  margin: 0 auto;
+  margin-top: 120px;
+  background-color: rgba(255, 255, 255, 0.425);
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  border-radius: 10px;
+}
+</style>
